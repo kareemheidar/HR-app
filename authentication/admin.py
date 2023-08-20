70c11ff66cd2012abd6513cbf9c622165dae976b
kareem <k.abokhater@gmail.com>
2023-08-20 10:41:59 +0300

    register

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import candidate, human_resources, job, candidate_account, department
+from django.contrib.auth.models import User
 
 # Register your models here.
 
 admin.site.site_header = "Admin Panel"
 admin.site.site_title = "HR Admin Panel"
 
+def register_candidate(modeladmin, request, queryset):
+    for candidate in queryset:
+        username = candidate.username
+        password = candidate.password
+        email = candidate.candID.email
+        newuser = User.objects.create_user(username, email, password)
+        newuser.save()
 
 @admin.register(human_resources) 
 class human_resources(admin.ModelAdmin):
@@ -19,7 +27,8 @@ class JobAdmin(admin.ModelAdmin):
 
 @admin.register(candidate_account)
 class CandidateAccountAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('username','password')
+    actions = [register_candidate]
 
 @admin.register(department)
 class DepartmentAdmin(admin.ModelAdmin):
