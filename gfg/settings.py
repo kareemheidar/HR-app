cfc9359ec37d94e28baea768a815fdd4d84e68a2
kareemheidar <k.abokhater@gmail.com>
2023-09-09 16:15:22 +0300

    as

--- gfg/settings.py
+++ gfg/settings.py
@@ -92,12 +92,14 @@ WSGI_APPLICATION = 'gfg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'madbase',
-        'USER': 'root',
-        'PASSWORD': 'DB@Madkour@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'madbase',
+        # 'USER': 'root',
+        # 'PASSWORD': 'DB@Madkour@123',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
--- gfg/wsgi.py
+++ gfg/wsgi.py
@@ -10,10 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from gfg.wsgi import authentication
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gfg.settings')
 
-application = authentication()
+application = get_wsgi_application()
 
 app = application
